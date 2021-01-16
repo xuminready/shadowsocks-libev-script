275a8ac6d63f51ae22a821ef453f380347ff2cf5
xuminready <xuminready@gmail.com>
2021-01-15 17:09:24 -0800

    Update enableBBR.sh

--- enableBBR.sh
+++ enableBBR.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#https://www.flyzy2005.com/fan-qiang/shadowsocks/ubuntu-bbr-shadowsocks/
+echo 'orginal source: https://www.flyzy2005.com/fan-qiang/shadowsocks/ubuntu-bbr-shadowsocks/'
+
+echo "use /usr/lib/sysctl.d/*.conf if /etc/sysctl.conf doesn't exist"
+echo 'https://wiki.archlinux.org/index.php/sysctl'
 
 sudo bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
 sudo bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
