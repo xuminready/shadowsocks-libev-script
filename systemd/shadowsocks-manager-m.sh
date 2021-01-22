8c25727b5b2bdf205a4f43583ed7160beb881b74
Min Xu <xuminready@gmail.com>
2021-01-22 10:16:44 -0800

    add systemd service configure file

---
+++ systemd/shadowsocks-manager-m.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+export NODE_HOME=`echo /opt/node-*`
+echo using ${NODE_HOME}
+export NODE_PATH=${NODE_HOME}/lib/node_modules
+export PATH=${PATH}:${NODE_HOME}/bin
+export LD_LIBRARY_PATH=${NODE_HOME}/lib
+export C_INCLUDE_PATH=${NODE_HOME}/include
+export CPLUS_INCLUDE_PATH=${NODE_HOME}/include
+
+node -v
+
+$SCRIPTPATH/shadowsocks-manager/bin/ssmgr -c $SCRIPTPATH/m_manager.yml
